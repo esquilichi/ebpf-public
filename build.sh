0e78f5b5ef3ce80d64c1b6e6d3d5886e0e147370
esquilichi <esquilichi@outlook.es>
2024-02-22 13:57:43 +0100

    clean commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+git clone --recurse-submodules https://github.com/libbpf/bpftool.git
+
+cd $HOME
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 428D7C01 C8CAB6595FDFF622
+printf "deb http://ddebs.ubuntu.com %s main restricted universe multiverse\n" $(lsb_release -cs){,-updates,-security,-proposed} |  sudo tee -a /etc/apt/sources.list.d/ddebs.list
+sudo apt update
+sudo apt install linux-tools-$(uname -r) bpftrace clang libbpf-dev linux-headers-$(uname -r) git llvm bpftrace-dbgsym pkg-config make
+
+
+git clone --recurse-submodules https://github.com/libbpf/bpftool.git
+cd bpftool/src/
+make
+sudo make install
+
+cd $HOME
+
+sudo ln -s $HOME/bpftool/libbpf/src/libbpf.so.1.4.0 /lib/x86_64-linux-gnu/libbpf.so.1
+
