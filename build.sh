2c008c774921ffff866b7e126b3ea4a7771003df
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2024-02-22 17:33:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd $HOME
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 428D7C01 C8CAB6595FDFF622
 printf "deb http://ddebs.ubuntu.com %s main restricted universe multiverse\n" $(lsb_release -cs){,-updates,-security,-proposed} |  sudo tee -a /etc/apt/sources.list.d/ddebs.list
 sudo apt update
-sudo apt install linux-tools-$(uname -r) bpftrace clang libbpf-dev linux-headers-$(uname -r) git llvm bpftrace-dbgsym pkg-config make
+sudo apt install linux-tools-$(uname -r) bpftrace clang libbpf-dev linux-headers-$(uname -r) git llvm bpftrace-dbgsym pkg-config make bpfcc-tools
 
 
 git clone --recurse-submodules https://github.com/libbpf/bpftool.git
